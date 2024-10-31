121ba4c1107edd75f551a2e2a9db858341d79ebd
Rokidna <qq617462323@outlook.com>
2024-10-31 12:27:39 +0800

    Finish MySQL DB build. Connect Sample Code and query

---
+++ src/comp4342/backend/Main.java
@@ -0,0 +1,34 @@
+package comp4342.backend;
+
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+//        1.加载驱动
+        Class.forName("com.mysql.cj.jdbc.Driver");
+//        2.用户信息和url
+        String url = "jdbc:mysql://192.168.31.188:3306/chat_app?useUnicode=true&characterEncoding=utf-8";
+        String username="root";
+        String password="SecretDB";
+//        3.连接成功，数据库对象 Connection
+        Connection connection = DriverManager.getConnection(url,username,password);
+//        4.执行SQL对象Statement，执行SQL的对象
+        Statement statement = connection.createStatement();
+//        5.执行SQL的对象去执行SQL，返回结果集
+        String sql = "SELECT * FROM user;";
+        ResultSet resultSet = statement.executeQuery(sql);
+        while (resultSet.next()) {
+            System.out.println("id = " + resultSet.getString("uid"));
+            System.out.println("Name = " + resultSet.getString("uname"));
+            System.out.println("Email = " + resultSet.getString("email"));
+            System.out.println("Password = " + resultSet.getString("password"));
+            System.out.println("Registration Time = " + resultSet.getString("regtime"));
+        }
+
+//        6.释放连接
+        resultSet.close();
+        statement.close();
+        connection.close();
+    }
+}
+
